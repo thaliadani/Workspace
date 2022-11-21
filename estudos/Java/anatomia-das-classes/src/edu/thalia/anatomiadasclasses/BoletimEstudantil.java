1bf87e42e8d58efd16cf9aeb4d9879c7827b2d6a
thaliadani <thaliadani2@gmail.com>
2022-11-21 16:47:49 -0300

    java-terminal

--- estudos/Java/anatomia-das-classes/src/edu/thalia/anatomiadasclasses/BoletimEstudantil.java
+++ estudos/Java/anatomia-das-classes/src/edu/thalia/anatomiadasclasses/BoletimEstudantil.java
@@ -1,22 +1,24 @@
 package edu.thalia.anatomiadasclasses;
+
 import javax.lang.model.util.ElementScanner14;
 
 /**
- * The class BoletimEstudantil has a main method that prints out a message based on the value of the
+ * The class BoletimEstudantil has a main method that prints out a message based
+ * on the value of the
  * variable mediaFinal
  */
 public class BoletimEstudantil {
     /**
      * The function takes in a number and prints out a message based on the number
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int mediaFinal = 7;
-            if(mediaFinal<6)
-                System.out.println("REPROVADO");
-            else if(mediaFinal==6)
-                System.out.println("PROVA MINERVA");
-            else 
-                System.out.println("APROVADO");
-     }
-    
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+
 }
