9f25df1fbc985a7395c136c1507484b67689e68e
thaliadani <thaliadani2@gmail.com>
2024-10-21 17:54:09 -0700

    new file:   Estudos/JS/variaveis-constantes/readme.md 	new file:   Estudos/JS/variaveis-constantes/script.js 	new file:   detona-ralph-game

---
+++ Estudos/JS/variaveis-constantes/script.js
@@ -0,0 +1,22 @@
+//let - e uma variavel, ou seja pode ser modificado
+//const - e uma constante que NAO pode ser modificado
+
+//strings
+const numeroTelefone = "(12)3456-7893"
+//numbers
+let idade = 23
+//booleans
+let aprovado = true
+
+console.log('meu número de telefone é: ' + numeroTelefone)
+console.log('minha idade é: ' + idade)
+console.log('meu status de aprovação é: ' + aprovado)
+
+idade = 24
+// numeroTelefone = "(12)2574-8955"
+
+console.log('minha nova idade é: ' + idade)
+console.log('meu novo número de telefone é: ' + numeroTelefone)
+
+
+
