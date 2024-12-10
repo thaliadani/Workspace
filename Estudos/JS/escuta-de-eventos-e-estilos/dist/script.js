38c779cf4d1886d02fe73a626f8bb1d45834d7f7
thaliadani <thaliadani2@gmail.com>
2024-12-10 18:11:50 -0300

    modified:   Estudos/JS/escuta-de-eventos-e-estilos/dist/script.js 	modified:   Estudos/JS/escuta-de-eventos-e-estilos/index.html 	modified:   Estudos/JS/escuta-de-eventos-e-estilos/script.ts

--- Estudos/JS/escuta-de-eventos-e-estilos/dist/script.js
+++ Estudos/JS/escuta-de-eventos-e-estilos/dist/script.js
@@ -4,9 +4,14 @@ const botaoDiminuir = document.querySelector("#diminuir");
 const botaoAumentar = document.querySelector("#aumentar");
 botaoAumentar === null || botaoAumentar === void 0 ? void 0 : botaoAumentar.addEventListener("click", () => {
     const valorAtual = Number(contador === null || contador === void 0 ? void 0 : contador.textContent);
-    contador.textContent = String(valorAtual + 1);
+    contador.textContent = (valorAtual + 1).toString();
 });
 botaoDiminuir === null || botaoDiminuir === void 0 ? void 0 : botaoDiminuir.addEventListener("click", () => {
     const valorAtual = Number(contador === null || contador === void 0 ? void 0 : contador.textContent);
-    contador.textContent = String(valorAtual - 1);
+    contador.textContent = (valorAtual - 1).toString();
+});
+////////////////////////////////////////////
+const input = document.querySelector("#campo-texto");
+input === null || input === void 0 ? void 0 : input.addEventListener("input", () => {
+    console.log(input.value);
 });
--- Estudos/JS/escuta-de-eventos-e-estilos/script.ts
+++ Estudos/JS/escuta-de-eventos-e-estilos/script.ts
@@ -1,18 +1,25 @@
 const contador = document.querySelector("#contador");
 
 const botaoDiminuir = document.querySelector("#diminuir");
-
 const botaoAumentar = document.querySelector("#aumentar");
 
 
 botaoAumentar?.addEventListener("click", () => {
     const valorAtual = Number(contador?.textContent);
 
-    contador!.textContent = String(valorAtual + 1);
+    contador!.textContent = (valorAtual + 1).toString();
 });
 
 botaoDiminuir?.addEventListener("click", () => {
     const valorAtual = Number(contador?.textContent);
 
-    contador!.textContent = String(valorAtual - 1);
+    contador!.textContent = (valorAtual - 1).toString();
+});
+
+////////////////////////////////////////////
+
+const input = document.querySelector("#campo-texto") as HTMLInputElement;
+
+input?.addEventListener("input", () => {
+    console.log(input.value);
 });
