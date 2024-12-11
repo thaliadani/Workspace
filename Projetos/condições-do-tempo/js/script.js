62afe53d62802358aa6de50ed4f96bd4aa732c5e
thaliadani <thaliadani2@gmail.com>
2024-12-11 15:40:01 -0300

    new file:   "Projetos/condi\303\247\303\265es-do-tempo/css/style.css" 	new file:   "Projetos/condi\303\247\303\265es-do-tempo/index.html" 	new file:   "Projetos/condi\303\247\303\265es-do-tempo/js/script.js" 	new file:   "Projetos/condi\303\247\303\265es-do-tempo/ts/script.ts" 	renamed:    Projetos/previsao-do-tempo/tsconfig.json -> "Projetos/condi\303\247\303\265es-do-tempo/tsconfig.json" 	deleted:    Projetos/previsao-do-tempo/css/style.css 	deleted:    Projetos/previsao-do-tempo/dist/script.js 	deleted:    Projetos/previsao-do-tempo/index.html 	deleted:    Projetos/previsao-do-tempo/script.ts

---
+++ Projetos/condições-do-tempo/js/script.js
@@ -0,0 +1,3 @@
+"use strict";
+// https://api.openweathermap.org/data/2.5/weather?q=Belo%20Horizonte&lang=pt_br&units=metric&appid=e8e2511e8fba822dabdc17679383a4e7
+// https://openweathermap.org/img/wn/02d@2x.png
--- Projetos/previsao-do-tempo/script.ts
+++
@@ -1,3 +0,0 @@
-https://api.openweathermap.org/data/2.5/weather?q=Belo%20Horizonte&lang=pt_br&units=metric&appid=e8e2511e8fba822dabdc17679383a4e7
-
-https://openweathermap.org/img/wn/02d@2x.png
