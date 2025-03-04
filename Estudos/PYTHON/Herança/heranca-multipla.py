5146f5ff2a4600d18d8d273312860e35168b0857
thaliadani <thaliadani2@gmail.com>
2025-03-04 14:29:33 -0300

    modified:   "Estudos/PYTHON/Heran\303\247a/heranca-multipla.py"

--- Estudos/PYTHON/Herança/heranca-multipla.py
+++ Estudos/PYTHON/Herança/heranca-multipla.py
@@ -18,15 +18,15 @@ class Animal :
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
         
 class Mamifero(Animal):
-    def __init__(self, nome, cor, peso, idade, amamentando):
-        super().__init__(nome, cor, peso, idade)
+    def __init__(self, amamentando, **kwargs):
+        super().__init__(**kwargs)
         self.amamentando = amamentando
     def amamentando(self):
         return f"{'Sim' if self.amamentando else 'Nao'} estou amamentando"
 
 class Ave(Animal):
-    def __init__(self, nome, cor, peso, idade,nadando):
-        super().__init__(nome, cor, peso, idade)
+    def __init__(self, nadando, **kwargs):
+        super().__init__(**kwargs)
         self.nadando = nadando
     def voando(self):
         return f"{self.get_nome()} esta voando"
@@ -48,20 +48,31 @@ class Pinguim(Ave):
 class Pato(Ave):
     def grasnando(self):
         return f"{self.get_nome()} esta grasnando"
-  
 
-cachorro1 = Cachorro("Cachorro", "Branco", 10, 5,'Sim')
+class Onitorrinco(Mamifero,Ave):
+    def __init__(self, nome, cor, peso, idade, amamentando, nadando):  
+        # print(Onitorrinco.__mro__)
+        print(Onitorrinco.mro())
+        
+        super().__init__(amamentando=amamentando, nadando=nadando, nome=nome, cor=cor, peso=peso, idade=idade)
+        
 
-gato1 = Gato("Gato", "Preto", 10, 5,'Nao')
 
-pinguim1 = Pinguim("Pinguim", "Branco", 10, 5,'Nao')
+cachorro1 = Cachorro(nome="Cachorro", cor="Branco", peso=10, idade=5,amamentando='Sim')
 
-pato1 = Pato("Pato", "Preto", 10, 5,'Sim')
+gato1 = Gato(nome="Gato", cor="Preto", peso=10, idade=5, amamentando='Nao')
+
+pinguim1 = Pinguim(nome="Pinguim", cor="Branco", peso=10, idade=5,nadando='Nao')
+
+pato1 = Pato(nome="Pato", cor="Preto", peso=10, idade=5,nadando='Sim')
+
+onitorrinco = Onitorrinco(nome="Onitorrinco", cor="Branco", peso=10, idade=5,amamentando='Sim', nadando='Sim')
 
 print(cachorro1)
 print(gato1)
 print(pinguim1)
 print(pato1)
+print(onitorrinco)
 
 print(cachorro1.comer())
 print(cachorro1.dormir())
