bec3751f3509ae8416ec6e8088ac96693a9b36fb
thaliadani <thaliadani2@gmail.com>
2025-03-05 00:11:31 -0300

    adiciona classe de polimorfismo com herança para Passaro e subclasses; atualiza configurações do cSpell

---
+++ Estudos/PYTHON/polimorfismo-com-heranca.py
@@ -0,0 +1,33 @@
+class Passaro:
+    def __init__(self, voar):
+        self.voar = voar
+        
+    def voar(self):
+        print('Passaro voando...')
+        
+class Pardal(Passaro):
+    def __init__(self):
+        super().__init__('Pardal')
+    def voar(self):
+        print('Pardal voando...')
+
+class Pinguim(Passaro):
+    def __init__(self):
+        super().__init__('Pinguim')
+    def voar(self):
+        print('Pinguim não voa')
+        
+
+class Aviao(Passaro):
+    def __init__(self):
+        super().__init__('Avião')
+    def voar(self):
+        print('Avião voando...')
+
+def plano_voo(obj):
+    obj.voar(Passaro)
+    
+
+plano_voo(Pardal)
+plano_voo(Pinguim)
+plano_voo(Aviao)
