8c472d9b91603fb1d50ff85a235cbb17417434ed
thaliadani <thaliadani2@gmail.com>
2025-05-09 15:30:09 -0300

    atualiza caminhos de áudio em index.html e implementa função para tocar sons ao clicar nas teclas

--- Projetos/aluramid/main.js
+++ Projetos/aluramid/main.js
@@ -1,3 +1,19 @@
-document.querySelectorAll('.teclas');
+function tocaSom(idElementoAudio) {
+  const elementoAudio = document.querySelector(idElementoAudio);
+  if (elementoAudio) {
+    elementoAudio.play();
+  } else {
+    console.error(`Elemento com o seletor "${idElementoAudio}" não encontrado.`);
+  }
+}
 
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (const tecla of listaDeTeclas){
+    const instumento = tecla.classList[1];
+    const idAudio = `#som_${instumento}`; // template string
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+      };
+}
 
