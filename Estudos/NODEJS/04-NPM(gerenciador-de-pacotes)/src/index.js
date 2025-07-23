7d188cceeb9f74a5ba543eac8277b9f252155b9f
thaliadani <thaliadani2@gmail.com>
2025-07-23 00:05:33 -0300

    modified:   Estudos/NODEJS/04-NPM(gerenciador-de-pacotes)/package-lock.json 	modified:   Estudos/NODEJS/04-NPM(gerenciador-de-pacotes)/package.json 	modified:   Estudos/NODEJS/04-NPM(gerenciador-de-pacotes)/src/index.js

--- Estudos/NODEJS/04-NPM(gerenciador-de-pacotes)/src/index.js
+++ Estudos/NODEJS/04-NPM(gerenciador-de-pacotes)/src/index.js
@@ -5,8 +5,14 @@
 
 //npm i - Restaurar o node_modules
 //npm i <pacote> - Instalar um pacote específico
+//npm i <pacote> -D - Instalar um pacote somente para desenvolvimento
+
 
 //node --watch index.js - Executar o arquivo index.js e reiniciar automaticamente ao salvar
 
 import chalk from 'chalk';
+import logSymbols from 'log-symbols';
+
 console.log(chalk.red.bgCyan.bold('Olá, mundo!'));
+
+console.log(logSymbols.sucess, chalk.green.italic("Servidor iniciou com sucesso"));
