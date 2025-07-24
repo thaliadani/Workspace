7a0a6df8c0ac6c5f37296763c1bc254b22d4d8de
thaliadani <thaliadani2@gmail.com>
2025-07-24 12:50:45 -0300

    Initialize TypeScript project with basic setup and configuration
    - Created package.json with scripts for building and running the project
    - Added TypeScript source file with a simple main function
    - Configured tsconfig.json for TypeScript compilation settings

---
+++ Estudos/TYPESCRIPT/01-typescript-com-nodejs/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let myName = "Thalia";
+  console.log(myName);
+}
+main();
---
+++ Estudos/TYPESCRIPT/01-typescript-com-nodejs/src/index.ts
@@ -0,0 +1,8 @@
+//npx tsc --init - Cria o tsconfig.json
+
+function main(){
+    let myName: string = "Thalia"
+    console.log(myName)
+}
+
+main()
