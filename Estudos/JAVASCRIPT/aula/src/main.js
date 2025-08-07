38f96984da5d69a80e186020627b1eac48b3b0a6
Thalia Danielle <thaliadani2@gmail.com>
2025-08-07 11:34:32 -0300

    Add files via upload

--- Estudos/JAVASCRIPT/aula/src/main.js
+++ Estudos/JAVASCRIPT/aula/src/main.js
@@ -9,3 +9,19 @@ function mostrarTexto(){
 function esconderTexto(){
     document.getElementById("mensagem").innerText ="Saiu da div";
 }
+
+function mostrarEscolha(fruta){
+    document.getElementById("resultado").innerText="Você escolheu: " + fruta;
+}
+
+function mostrarTextoDigitado(texto){
+    document.getElementById("texto").innerText= texto;
+}
+
+function teclaPressionada(event){
+    document.getElementById("tecla").innerText ="Tecla pressionada: " + event.key;
+}
+
+function teclaLiberada(event){
+    document.getElementById("tecla-up").innerText ="Tecla pressionada: " + event.key;
+}
