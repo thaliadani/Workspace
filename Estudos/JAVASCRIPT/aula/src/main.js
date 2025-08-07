a04fe8c93202dd3f32c880e5db5e772719750951
Thalia Danielle <thaliadani2@gmail.com>
2025-08-07 09:51:18 -0300

    Add files via upload

---
+++ Estudos/JAVASCRIPT/aula/src/main.js
@@ -0,0 +1,11 @@
+function mostrarMensagem(){
+    alert("Você clicou no botão de teste!");
+}
+
+function mostrarTexto(){
+    document.getElementById("mensagem").innerText ="Entrou na div";
+}
+
+function esconderTexto(){
+    document.getElementById("mensagem").innerText ="Saiu da div";
+}
