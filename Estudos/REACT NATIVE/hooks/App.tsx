1d0cfba6aedf2256a573c49b89b3e3b79c333747
Thalia Danielle <thaliadani2@gmail.com>
2025-08-14 15:59:49 -0300

    modified:   Estudos/REACT NATIVE/hooks/App.tsx 	new file:   Estudos/REACT NATIVE/hooks/App2.tsx

--- Estudos/REACT NATIVE/hooks/App.tsx
+++ Estudos/REACT NATIVE/hooks/App.tsx
@@ -1,24 +1,80 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, useReducer, use } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import {App2} from './App2';
 
 export default function App() {
+
+  //TODO useReducer - Use em componentes mais complexos
+
+  /* 
+      const reducer = (state: { counter: number }, action: { type: string }) => {
+        switch (action.type) {
+          case 'INCREMENT':
+            return { counter: state.counter + 1 };
+          case 'DECREMENT':
+            if (state.counter > 0) {
+              return { counter: state.counter - 1 };
+            }
+          default:
+            return state;
+        }
+      }
+  
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  
+      const incrementCount = () => {
+        dispatch({ type: 'INCREMENT' });
+      }
+  
+      const decrementCount = () => {
+        dispatch({ type: 'DECREMENT' });
+      }
+  
+      useEffect(() => {
+        if(state.counter === 0){
+          Alert.alert('Carrinho', 'Carrinho vazio');
+        }else{
+          Alert.alert('Carrinho', 'quantidade: ' + state.counter);
+        }
+      }, [state.counter])
+  */
+
+  //TODO useState - Use em pequenos componentes
+
   const [count, setCount] = useState(0);
 
-  const incrementCount= () => {
+  useEffect(() => {
+    if (count === 0) {
+      Alert.alert('Carrinho', 'Carrinho vazio');
+    } else {
+      Alert.alert('Carrinho', 'quantidade: ' + count);
+    }
+  }, [count])
+
+  const incrementCount = () => {
     setCount((prevState) => prevState + 1);
   }
 
-  const decrementCount= () => {
-    setCount((prevState) => prevState - 1);
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount((prevState) => prevState - 1);
+    }
   }
 
   return (
     <View style={styles.container}>
+      <Text style={styles.big}>hooks</Text>
+
       <Text style={styles.big}>{count}</Text>
 
       <View style={styles.inline}>
         <Button title='Remover' onPress={decrementCount}></Button>
         <Button title='Adicionar' onPress={incrementCount}></Button>
+      </View>
+
+
+      <View style={styles.inline}>
+        <App2 />
       </View>
     </View>
   );
@@ -30,11 +86,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
   },
   big: {
     fontSize: 50,
-  }, 
+  },
   inline: {
     flexDirection: 'row',
+    gap: 10,
+    marginTop: 40
   }
 });
