5b50ca8c2a94bf332c538e3608a21eeda0961733
Thalia Danielle <31187727+thaliadani@users.noreply.github.com>
2025-10-17 09:09:17 -0300

    Add files via upload

---
+++ Estudos/PHP/backend-manha/processa.php
@@ -0,0 +1,16 @@
+<?php
+$nome = $_POST['nome'] ?? '';
+$email = $_POST['email'] ?? '';
+$mensagem = $_POST['mensagem'] ?? '';
+
+if(empty($nome) || empty($email) || empty($mensagem)) {
+    echo "⚠️ Todos os campos são obrigatórios.";
+    exit;
+}
+
+echo "✅ Obrigado, <strong>$nome</strong>!<br>";
+echo "❤️ Recebemos sua mensagem:<br>";
+echo "<em>\"$mensagem\"</em><br><br>";
+echo "✉️ Entraremos em contato pelo email: <strong>$email</strong>.";
+
+?>
