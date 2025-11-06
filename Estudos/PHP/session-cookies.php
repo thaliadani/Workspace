ba9a4f6b286bafe5d03b9f9f40262af85f681d09
Thalia Danielle <31187727+thaliadani@users.noreply.github.com>
2025-11-06 11:03:28 -0300

    php - sessions e cookies

---
+++ Estudos/PHP/session-cookies.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    if(isset($_POST["usuario"])){
+        $_SESSION["usuario"] = $_POST["usuario"],time() -3600);
+        echo "Bem vindo, ". $_SESSION["usuario"];
+    }else{
+        ?>
+        <form method="POST">
+            Nome:<input type="text" name="usuario">
+            <input type="submit" value="Entrar">
+        </form>
+
+        <?php
+        if(isset($_COOKIE["ultimo_usuario"])){
+            echo "Ultimo usuario logado" . $_COOKIE["ultimo_usuario"];
+        }
+        ?>
+    }
+?>
